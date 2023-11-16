69b1dc6891850100727da91270bd8b09d7f6b723
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-11-16 15:41:04 -0800

    Adding basic schema and models for user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,14 @@
 // Import models
 const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 // Declare resolvers
-const resolvers = {};
-
+const resolvers = {
+  Query: {
+    user: async (parent, { username }) => {
+      return await User.findOne({ username });
+    },
+  },
+};
 // Export resolvers
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,18 @@
-const typeDefs = ``;
+const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
+  
+  type Query {
+    user: User
+  }
+  
+  type Auth {
+    token: ID
+    user: User
+  }
+  `;
 module.exports = typeDefs;
