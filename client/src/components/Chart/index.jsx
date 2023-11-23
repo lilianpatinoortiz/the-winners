09b0a65e4f9a9539f4b5697e60fcf7185424fb91
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-11-23 00:53:54 -0800

    Adding recharts

---
+++ client/src/components/Chart/index.jsx
@@ -0,0 +1,59 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Label,
+  ResponsiveContainer,
+} from "recharts";
+import { useTheme } from "@mui/material/styles";
+
+function Chart({ data }) {
+  const theme = useTheme();
+
+  return (
+    <>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{
+            top: 16,
+            right: 16,
+            bottom: 0,
+            left: 24,
+          }}
+        >
+          <XAxis
+            dataKey="time"
+            stroke={theme.palette.text.secondary}
+            style={theme.typography.body2}
+          />
+          <YAxis
+            stroke={theme.palette.text.secondary}
+            style={theme.typography.body2}
+          >
+            <Label
+              angle={270}
+              position="left"
+              style={{
+                textAnchor: "middle",
+                fill: theme.palette.text.primary,
+                ...theme.typography.body1,
+              }}
+            >
+              Tasks Completed
+            </Label>
+          </YAxis>
+          <Line
+            isAnimationActive={false}
+            type="monotone"
+            dataKey="amount"
+            stroke={theme.palette.primary.main}
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
+}
+export { Chart };
--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -5,55 +5,54 @@ import HomeIcon from "@mui/icons-material/Home";
 import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
 
 function Navbar() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
     <Nav
-      links={[
-        <Button
-          key={1}
-          variant="text"
-          href="/"
-          startIcon={<HomeIcon />}
-          size="large"
-        >
-          Home
-        </Button>,
-        <Button
-          key={2}
-          variant="text"
-          href="/myguru"
-          startIcon={<AccessTimeFilledIcon />}
-          size="large"
-        >
-          My Guru
-        </Button>,
-        <Button
-          key={3}
-          variant="text"
-          href="/kanban"
-          startIcon={<CalendarMonthIcon />}
-          size="large"
-        >
-          Kanban
-        </Button>,
-        <Button
-          key={4}
-          variant="text"
-          href="/tasks"
-          startIcon={<TaskAltIcon />}
-          size="large"
-        >
-          Tasks
-        </Button>,
-      ]}
       accessLinks={[
-        <Button key={5} variant="text" href="/login">
+        <Button key={0} variant="text" href="/login">
           Login
         </Button>,
-        <Button key={6} variant="text" href="/signup">
+        <Button key={1} variant="text" href="/signup">
           Signup
+        </Button>,
+      ]}
+      links={[
+        <Button key={2} href="/">
+          <ListItemAvatar>
+            <Avatar>
+              <HomeIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Home" />
+        </Button>,
+        <Button key={3} href="/myguru">
+          <ListItemAvatar>
+            <Avatar>
+              <AccessTimeFilledIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="My Guru" />
+        </Button>,
+        <Button key={4} href="/kanban">
+          <ListItemAvatar>
+            <Avatar>
+              <CalendarMonthIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Kanban" />
+        </Button>,
+        <Button key={5} href="/tasks">
+          <ListItemAvatar>
+            <Avatar>
+              <TaskAltIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Tasks" />
         </Button>,
       ]}
     ></Nav>
@@ -62,42 +61,44 @@ function Navbar() {
 
 function Nav({ links, accessLinks }) {
   return (
-    <div className="MuiDrawer-root MuiDrawer-docked css-1fqw99a">
-      <div className="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiDrawer-paper MuiDrawer-paperAnchorLeft MuiDrawer-paperAnchorDockedLeft css-1l8j5k8">
-        <div className="MuiToolbar-root MuiToolbar-gutters MuiToolbar-regular css-e7x0wp">
-          <button
-            className="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk"
-            tabIndex="0"
-            type="button"
-          >
-            <svg
-              className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
-              focusable="false"
-              aria-hidden="true"
-              viewBox="0 0 24 24"
-              data-testid="ChevronLeftIcon"
+    <>
+      <div className="MuiDrawer-root MuiDrawer-docked css-1fqw99a">
+        <div className="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiDrawer-paper MuiDrawer-paperAnchorLeft MuiDrawer-paperAnchorDockedLeft css-1l8j5k8">
+          <div className="MuiToolbar-root MuiToolbar-gutters MuiToolbar-regular css-e7x0wp">
+            <button
+              className="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk"
+              tabIndex="0"
+              type="button"
             >
-              <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
-            </svg>
-            <span className="MuiTouchRipple-root css-w0pj6f"></span>
-          </button>
-        </div>
-        <hr className="MuiDivider-root MuiDivider-fullWidth css-39bbo6"></hr>
-        <div className="MuiList-root MuiList-padding css-1ontqvh">
-          <h4 className="padding20"> Menu </h4>
-          <Stack spacing={2} direction="column">
-            {links.map((link) => link)}
-          </Stack>
-        </div>
-        <hr className="MuiDivider-root MuiDivider-fullWidth css-39bbo6"></hr>
-        <div className="MuiList-root MuiList-padding css-1ontqvh">
-          <h4 className="padding20"> Access </h4>
-          <Stack spacing={2} direction="column">
-            {accessLinks.map((link) => link)}
-          </Stack>
+              <svg
+                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                data-testid="ChevronLeftIcon"
+              >
+                <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
+              </svg>
+              <span className="MuiTouchRipple-root css-w0pj6f"></span>
+            </button>
+          </div>
+          <hr className="MuiDivider-root MuiDivider-fullWidth css-39bbo6"></hr>
+          <div className="MuiList-root MuiList-padding css-1ontqvh">
+            <h4 className="padding20"> Menu </h4>
+            <Stack spacing={2} direction="column">
+              {links.map((link) => link)}
+            </Stack>
+          </div>
+          <hr className="MuiDivider-root MuiDivider-fullWidth css-39bbo6"></hr>
+          <div className="MuiList-root MuiList-padding css-1ontqvh">
+            <h4 className="padding20"> Access </h4>
+            <Stack spacing={2} direction="column">
+              {accessLinks.map((link) => link)}
+            </Stack>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
