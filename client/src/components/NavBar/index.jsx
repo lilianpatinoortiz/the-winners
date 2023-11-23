d0404ad1f933da387adcda178a4c20a7a8b5097b
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-11-22 19:56:24 -0800

    Adding icons to navbar

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,35 +1,66 @@
 // Bringing in the required import from 'react-router-dom'
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
 
 function Navbar() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
     <Nav
       links={[
-        <Button key={1} variant="outlined" href="/">
+        <Button
+          key={1}
+          variant="text"
+          href="/"
+          startIcon={<HomeIcon />}
+          size="large"
+        >
           Home
         </Button>,
-        <Button key={2} variant="outlined" href="/myguru">
+        <Button
+          key={2}
+          variant="text"
+          href="/myguru"
+          startIcon={<AccessTimeFilledIcon />}
+          size="large"
+        >
           My Guru
         </Button>,
-        <Button key={3} variant="outlined" href="/kanban">
+        <Button
+          key={3}
+          variant="text"
+          href="/kanban"
+          startIcon={<CalendarMonthIcon />}
+          size="large"
+        >
           Kanban
         </Button>,
-        <Button key={4} variant="outlined" href="/tasks">
+        <Button
+          key={4}
+          variant="text"
+          href="/tasks"
+          startIcon={<TaskAltIcon />}
+          size="large"
+        >
           Tasks
         </Button>,
-        <Button key={5} variant="outlined" href="/login">
+      ]}
+      accessLinks={[
+        <Button key={5} variant="text" href="/login">
           Login
         </Button>,
-        <Button key={6} variant="outlined" href="/signup">
+        <Button key={6} variant="text" href="/signup">
           Signup
         </Button>,
       ]}
     ></Nav>
   );
 }
-function Nav({ links }) {
+
+function Nav({ links, accessLinks }) {
   return (
     <div className="MuiDrawer-root MuiDrawer-docked css-1fqw99a">
       <div className="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiDrawer-paper MuiDrawer-paperAnchorLeft MuiDrawer-paperAnchorDockedLeft css-1l8j5k8">
@@ -53,8 +84,16 @@ function Nav({ links }) {
         </div>
         <hr className="MuiDivider-root MuiDivider-fullWidth css-39bbo6"></hr>
         <div className="MuiList-root MuiList-padding css-1ontqvh">
+          <h4 className="padding20"> Menu </h4>
           <Stack spacing={2} direction="column">
             {links.map((link) => link)}
+          </Stack>
+        </div>
+        <hr className="MuiDivider-root MuiDivider-fullWidth css-39bbo6"></hr>
+        <div className="MuiList-root MuiList-padding css-1ontqvh">
+          <h4 className="padding20"> Access </h4>
+          <Stack spacing={2} direction="column">
+            {accessLinks.map((link) => link)}
           </Stack>
         </div>
       </div>
