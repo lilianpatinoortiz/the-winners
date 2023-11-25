e3721ed69b137d03d9c87616ef4d1780a1cb8f8a
Devon Hua <huadevon@gmail.com>
2023-11-24 18:48:06 -0600

    kanban board with editable feature create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Board from "./assets/components/KanbanBoard/Board";
+import Editable from "./assets/components/Editable/Editable";
+import { DragDropContext } from "react-beautiful-dnd";
+import { v4 as uuidv4 } from "uuid";
+import "../bootstrap.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [data, setData] = useState(
+    localStorage.getItem("kanban-board")
+      ? JSON.parse(localStorage.getItem("kanban-board"))
+      : []
+  );
+
+  const setName = (title, bid) => {
+    const index = data.findIndex((item) => item.id === bid);
+    const tempData = [...data];
+    tempData[index].boardName = title;
+    setData(tempData);
+  };
+
+  const dragCardInBoard = (source, destination) => {
+    let tempData = [...data];
+    const destinationBoardIdx = tempData.findIndex(
+      (item) => item.id.toString() === destination.droppableId
+    );
+    const sourceBoardIdx = tempData.findIndex(
+      (item) => item.id.toString() === source.droppableId
+    );
+    tempData[destinationBoardIdx].card.splice(
+      destination.index,
+      0,
+      tempData[sourceBoardIdx].card[source.index]
+    );
+    tempData[sourceBoardIdx].card.splice(source.index, 1);
+
+    return tempData;
+
+    const addBoard = (title) => {
+      const tempData = [...data];
+      tempData.push({
+        id: uuidv4(),
+        boardName: title,
+        card: [],
+      });
+      setData(tempData);
+    };
+  
+    const removeBoard = (bid) => {
+      const tempData = [...data];
+      const index = data.findIndex((item) => item.id === bid);
+      tempData.splice(index, 1);
+      setData(tempData);
+    };
+
+    const onDragEnd = (result) => {
+      const { source, destination } = result;
+      if (!destination) return;
+  
+      if (source.droppableId === destination.droppableId) return;
+  
+      setData(dragCardInBoard(source, destination));
+    };
+
+    useEffect(() => {
+      localStorage.setItem("kanban-board", JSON.stringify(data));
+    }, [data]);
+  
+  
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App" data-theme={theme}>
+        <Navbar switchTheme={switchTheme} />
+        <div className="app_outer">
+          <div className="app_boards">
+            {data.map((item) => (
+              <Board
+                key={item.id}
+                id={item.id}
+                name={item.boardName}
+                card={item.card}
+                setName={setName}
+                addCard={addCard}
+                removeCard={removeCard}
+                removeBoard={removeBoard}
+                updateCard={updateCard}
+              />
+            ))}
+            <Editable
+              class={"add__board"}
+              name={"Add Board"}
+              btnName={"Add Board"}
+              onSubmit={addBoard}
+              placeholder={"Enter Board  Title"}
+            />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </DragDropContext>
+  );
 }
 
 export default App
