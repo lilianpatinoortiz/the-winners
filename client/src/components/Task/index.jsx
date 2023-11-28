710fe97632958727387c50f575c39502f966f9e3
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-11-28 14:57:28 -0800

    modal for prjects and tasks, styling

--- client/src/components/Task/index.jsx
+++ client/src/components/Task/index.jsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
+ import { Button as MuiButton} from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { TasksList } from "../TasksList";
 import { rootShouldForwardProp } from "@mui/material/styles/styled";
-import { Modal } from "react-bootstrap";
+import { Form, Modal, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { ADD_TASK } from "../../utils/mutations";
 import { useMutation, useQuery } from "@apollo/client";
 import { QUERY_ME } from "../../utils/queries";
 import { useNavigate } from "react-router-dom";
-
-
-
 
 function TaskForm() {
   const navigate = useNavigate();
@@ -20,14 +17,23 @@ function TaskForm() {
   // Logged user data (me)
   const { loading: userLoading, data: userData } = useQuery(QUERY_ME);
   const user = userData?.me || {};
-  const [taskFormData, setTaskFormData] = useState({ title: "", description: "", createdDate: new Date(), dueDate: new Date(), priority: 1, status: "Open", project: "", userid: user._id});
+  const [taskFormData, setTaskFormData] = useState({
+    title: "",
+    description: "",
+    createdDate: new Date(),
+    dueDate: new Date(),
+    priority: 1,
+    status: "Open",
+    project: "",
+    userid: user._id,
+  });
   const [addTask, { error }] = useMutation(ADD_TASK);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setTaskFormData({ ...taskFormData, [name]: value });
   };
-  
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -51,113 +57,125 @@ function TaskForm() {
 
     setTaskFormData({
       title: "",
-       description: "", 
-       createdDate: new Date(),
-    dueDate: new Date(),
-       priority: 1,
-         status: "Open", 
-        project: "",
-        userid: user._id
-
+      description: "",
+      createdDate: new Date(),
+      dueDate: new Date(),
+      priority: 1,
+      status: "Open",
+      project: "",
+      userid: user._id,
     });
     navigate("/"); // Redirect user to home page
-
   };
-
 
   return (
     <div>
-      <form
+      <Form
         className="flex-row justify-center justify-space-between-md align-center"
         onSubmit={handleFormSubmit}
       >
-        <label>Title</label>
-        <input 
-          placeholder="title"
-          type="text"
-          name="title"
-          value={taskFormData.title}
-          onChange={(e) => setTaskFormData({ ...taskFormData, title: e.target.value })}
-        />
-        <label>Description</label>
-        <input 
-          placeholder="description"
-          type="text"
-          name="description"
-          onChange={(e) => setTaskFormData({ ...taskFormData, description: e.target.value })}
-          value={taskFormData.description}
-        />
-        <label>Due Date</label>
-        <input 
-          placeholder="due date"
-          type="date"
-          onChange={(e) => setTaskFormData({ ...taskFormData, dueDate: e.target.value })}
-          value={taskFormData.dueDate}
-        />
-        <label>Prority</label>
-        <input
-          placeholder="prority"
-          type="number"
-          min="1"
-          max="3"
-          name="prority"
-          onChange={(e) => setTaskFormData({ ...taskFormData, priority: e.target.value })}
-          value={taskFormData.priority}
-        />
-        <label>Project</label>
-        <input 
-          placeholder="project"
-          type="text"
-          name="project"
-          onChange={(e) => setTaskFormData({ ...taskFormData, project: e.target.value })}
-          value={taskFormData.project}
-        />
-        <Button
-          type="submit"
-          variant="success"
-        >
+        <Form.Group className="mb-3" controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            placeholder="title"
+            type="text"
+            name="title"
+            value={taskFormData.title}
+            onChange={(e) =>
+              setTaskFormData({ ...taskFormData, title: e.target.value })
+            }
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            placeholder="description"
+            type="text"
+            name="description"
+            onChange={(e) =>
+              setTaskFormData({ ...taskFormData, description: e.target.value })
+            }
+            value={taskFormData.description}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="dueDate">
+          <Form.Label>Due Date</Form.Label>
+          <Form.Control
+            placeholder="due date"
+            type="date"
+            onChange={(e) =>
+              setTaskFormData({ ...taskFormData, dueDate: e.target.value })
+            }
+            value={taskFormData.dueDate}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="priority">
+          <Form.Label>Prority</Form.Label>
+          <Form.Range
+            placeholder="prority"
+            type="number"
+            min="1"
+            max="3"
+            name="prority"
+            onChange={(e) =>
+              setTaskFormData({ ...taskFormData, priority: e.target.value })
+            }
+            value={taskFormData.priority}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="project">
+          <Form.Label>Project</Form.Label>
+          <Form.Control
+            placeholder="project"
+            type="text"
+            name="project"
+            onChange={(e) =>
+              setTaskFormData({ ...taskFormData, project: e.target.value })
+            }
+            value={taskFormData.project}
+          />
+        </Form.Group>
+        <Button type="submit" variant="secondary">
           Submit
         </Button>
-      </form>
+      </Form>
     </div>
-    
   );
 }
 
 function TasksContainer({ loading, rows, rowsPerPageProp }) {
-  
-  const [show, setShow] = useState({ task: false  });
+  const [show, setShow] = useState({ task: false });
 
-const createTask = () => setShow({ task: false });
-const handleShowCreateTask = () => setShow({ task: true });
-const handleClose = () => setShow({ task: false });
+  const createTask = () => setShow({ task: false });
+  const handleShowCreateTask = () => setShow({ task: true });
+  const handleClose = () => setShow({ task: false });
 
-  
-return (
+  return (
     <>
       {!loading ? (
         <>
-        <div id="modals">
-          <Modal show={show.task} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Create Task</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <TaskForm/>
-            </Modal.Body>
-          </Modal>
+          <div id="modals">
+            <Modal show={show.task} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Create Task</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <TaskForm />
+              </Modal.Body>
+            </Modal>
           </div>
           <Grid container spacing={2}>
             <Grid item xs={4} md={10}></Grid>
             <Grid item xs={8} md={2}>
-              <Button
-                component="label"
+              <MuiButton
+                // component="label"
                 variant="contained"
                 startIcon={<AddCircleIcon />}
-                onClick={handleShowCreateTask} id="login"
+                onClick={handleShowCreateTask}
+                id="login"
               >
                 Create task
-              </Button>
+              </MuiButton>
             </Grid>
           </Grid>
           {rows.length ? (
